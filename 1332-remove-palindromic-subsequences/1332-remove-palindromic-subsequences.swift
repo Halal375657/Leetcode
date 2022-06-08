373fa0f1a297473f9e29299e5597a2404d7abfb0
Halal Uddin <42236181+Halal375657@users.noreply.github.com>
2022-06-08 18:58:05 +0600

    Time: 42 ms (100.00%), Space: 14.4 MB (100.00%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.swift
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.swift
@@ -1,25 +1,23 @@
 class Solution {
     func removePalindromeSub(_ s: String) -> Int {
-        if isPalindrome(s) {
-            return 1
-        }
-        return 2
+        return isPalindrome(s) ? 1 : 2
     }
     
     func isPalindrome(_ input: String) -> Bool {
-      guard input.count > 1 else { return true }
-      var lhs = 0; var rhs = input.count - 1
-
-      while lhs < rhs {
-        guard input[lhs] == input[rhs] else { return false }
-
-        lhs += 1; rhs -= 1
-      }
-
-      return true
+        guard input.count > 1 else { return true }
+        var lhs = 0; var rhs = input.count - 1
+        
+        while lhs < rhs {
+            guard input[lhs] == input[rhs] else { return false }
+            
+            lhs += 1; rhs -= 1
+        }
+        
+        return true
     }
 }
 
+/// The extension method making accessable using index(like: `s[index]`) of a `String
 extension StringProtocol {
-  subscript(_ offset: Int) -> Element { self[index(startIndex, offsetBy: offset)] }
+    subscript(_ offset: Int) -> Element { self[index(startIndex, offsetBy: offset)]}
 }
