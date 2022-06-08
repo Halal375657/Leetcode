f09f7af507876429bf978da719118b0f9332422e
Halal Uddin <42236181+Halal375657@users.noreply.github.com>
2022-06-08 21:31:27 +0600

    Time: 51 ms (100.00%), Space: 14.4 MB (100.00%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.swift
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.swift
@@ -1,23 +1,34 @@
 class Solution {
+    
+    /// Find the minimum number of steps(remove palindromic subsequence) to make the string empty.
+    ///
+    /// - parameters:
+    ///  - input: The `String` consisting only letters `a` and `b`.
+    /// - returns: return value would be `true` if is it palinmrome string nor `false`
     func removePalindromeSub(_ s: String) -> Int {
         return isPalindrome(s) ? 1 : 2
     }
     
+    /// Identify a palindrome string. Complexity: O(n)
+    /// - parameters:
+    ///  - input: The `String` consisting only letters `a` and `b`.
+    /// - returns: return value would be `true` if is it palinmrome string nor `false`
     func isPalindrome(_ input: String) -> Bool {
         guard input.count > 1 else { return true }
         var lhs = 0; var rhs = input.count - 1
-        
+
         while lhs < rhs {
             guard input[lhs] == input[rhs] else { return false }
-            
-            lhs += 1; rhs -= 1
+
+            lhs += 1; rhs -= 1;
         }
-        
+
         return true
     }
 }
 
-/// The extension method making accessable using index(like: `s[index]`) of a `String
+/// The extension subscript making accessable a String using index(like: `s[index]`).
 extension StringProtocol {
     subscript(_ offset: Int) -> Element { self[index(startIndex, offsetBy: offset)]}
 }
+
