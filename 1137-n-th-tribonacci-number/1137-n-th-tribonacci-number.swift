36a7e0c7dc390c279973075e7a66c68dc2c40fdf
Halal Uddin <42236181+Halal375657@users.noreply.github.com>
2022-08-28 12:01:22 +0600

    Time: 3 ms (64.18%), Space: 13.2 MB (95.52%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.swift
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.swift
@@ -1,16 +1,17 @@
 class Solution {
+    
+    var dpTable: [Int] = [0, 1, 1]
+    
+    init() {
+        for i in 3..<37+1 {
+            self.dpTable.append(dpTable[i-1] + dpTable[i-2] + dpTable[i-3])
+        }
+    }
+    
     func tribonacci(_ n: Int) -> Int {
         if n == 0 { return 0}
         if n == 1 { return 1}
         
-        var res = 0
-        var t1 = 0; var t2 = 1; var t3 = 1;
-        for i in 2..<n {
-            var temp = t3
-            t3 = t1 + t2 + t3
-            t1 = t2; t2 = temp
-        }
-        
-        return t3
+        return self.dpTable[n]
     }
 }
