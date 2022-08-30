da8a81c7c427dc20b2995ced3c2c61f97b4e47fe
Halal Uddin <42236181+Halal375657@users.noreply.github.com>
2022-08-30 21:52:14 +0600

    Time: 3 ms (64.99%), Space: 13.6 MB (77.72%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.swift
+++ 70-climbing-stairs/70-climbing-stairs.swift
@@ -1,14 +1,14 @@
 class Solution {
     
-    var result: Array<Int> = [0,1,2]
+    var lookup: Array<Int> = [0,1,2]
     
     init() {
         for i in 3..<46 {
-            result.append(result[i-1] + result[i-2])
+            lookup.append(lookup[i-1] + lookup[i-2])
         }
     }
     
     func climbStairs(_ n: Int) -> Int {
-        return result[n]
+        return lookup[n]
     }
 }
