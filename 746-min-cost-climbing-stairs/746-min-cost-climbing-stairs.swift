a7d9b3856f37feacf2ff8d321fc47d10550f19a3
Halal Uddin <42236181+Halal375657@users.noreply.github.com>
2022-09-18 16:34:30 +0600

    Time: 29 ms (80.11%), Space: 14 MB (63.64%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.swift
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.swift
@@ -4,7 +4,7 @@ class Solution {
         // (0, 0) -> (0, 1) -> (1, 100) -> (100, 2) -> (2, 3) -> (3, 3) -> (3, 103) -> (103, 4) -> (4, 5) -> (5, 104) -> (5, 6)
         
         var first = 0; var second = 0;
-        for (index, val) in cost.enumerated() {
+        for val in cost{
             var temp = second
             second = val + min(first, second)
             first = temp
